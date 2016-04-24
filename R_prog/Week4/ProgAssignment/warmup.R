29655537d14b52bd8d5dd356dfb10e4910ed65a3
Aliki Mavromoustaki <kiki@Alikis-MacBook-Pro-2.local>
2016-04-24 23:18:12 +0100

    del rpr

--- R_prog/Week4/ProgAssignment/warmup.R
+++
@@ -1,7 +0,0 @@
-# Part 1 (ungraded)
-
-outcome<-read.csv('outcome-of-care-measures.csv', colClasses = 'character')
-head(outcome)
-
-outcome[,11]<-as.numeric(outcome[,11]) # we read the data as character (line 3) so we need to coerce the column to be numeric
-
